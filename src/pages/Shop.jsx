a8b204e9a18aa0390fc47aa0309b47cc718f6b11
AkarshSahlot <122122123+AkarshSahlot@users.noreply.github.com>
2024-06-24 23:20:54 +0530

    Update Shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -4,18 +4,18 @@ import ProductCard from '../componenets/ProductCard';
 const products = [
   { id: 1, name: 'Canvas 12x12', price: 10.99 },
   { id: 2, name: 'Canvas 16x20', price: 15.99 },
-  { id: 2, name: 'Canvas 16x20', price: 15.99 },
-  { id: 2, name: 'Canvas 16x20', price: 15.99 },
-  { id: 2, name: 'Canvas 16x20', price: 15.99 },
-  { id: 2, name: 'Canvas 16x20', price: 15.99 },
-  { id: 2, name: 'Canvas 16x20', price: 15.99 },
-  { id: 2, name: 'Canvas 16x20', price: 15.99 },
-  { id: 2, name: 'Canvas 16x20', price: 15.99 },
-  { id: 2, name: 'Canvas 16x20', price: 15.99 },
-  { id: 2, name: 'Canvas 16x20', price: 15.99 },
+  { id: 3, name: 'Canvas 16x20', price: 15.99 },
+  { id: 4, name: 'Canvas 16x20', price: 15.99 },
+  { id: 5, name: 'Canvas 16x20', price: 15.99 },
+  { id: 6, name: 'Canvas 16x20', price: 15.99 },
+  { id: 7, name: 'Canvas 16x20', price: 15.99 },
+  { id: 8, name: 'Canvas 16x20', price: 15.99 },
+  { id: 9, name: 'Canvas 16x20', price: 15.99 },
+  { id: 10, name: 'Canvas 16x20', price: 15.99 },
+  { id: 11, name: 'Canvas 16x20', price: 15.99 },
 
-  // Add more products as needed
-];
+  // Add more products as needed\
+  ];
 
 const Shop = () => (
   <Container>
