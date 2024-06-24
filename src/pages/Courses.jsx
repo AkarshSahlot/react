abdd7b4e6179005dea2a4bb8393b42a65911e2fb
AkarshSahlot <122122123+AkarshSahlot@users.noreply.github.com>
2024-06-24 23:16:52 +0530

    Update Courses.jsx

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -5,6 +5,13 @@ import CourseCard from '../componenets/CourseCard';
 const courses = [
   { id: 1, title: 'Watercolor Painting', description: 'Learn the basics of watercolor painting.' },
   { id: 2, title: 'Oil Painting', description: 'Advanced techniques in oil painting.' },
+  { id: 2, title: 'Natural Painting', description: 'Advanced techniques in  painting.' },
+  { id: 2, title: 'Abstract Painting', description: 'Advanced techniques in  painting.' },
+  { id: 2, title: 'PastelColor Painting', description: 'Advanced techniques in painting.' },
+  { id: 2, title: 'Anime Painting', description: 'Advanced techniques in  painting.' },
+  { id: 2, title: 'yash Nadar Painting', description: 'Advanced techniques in  painting.' },
+  { id: 2, title: 'Oil Painting', description: 'Advanced techniques in  painting.' },
+
   // Add more courses as needed
 ];
 
